8a852a5847a18f6d6274257fc58d8025ed14a0d4
Ishu <kumarishu494@gmail.com>
2024-04-19 15:21:41 +0530

    Basic homepage

---
+++ products.js
@@ -0,0 +1,9 @@
+const PRODUCTS = [
+    { id: 1, name: "Apple", category: "Fruit" },
+    { id: 2, name: "Avocado", category: "Fruit" },
+    { id: 3, name: "Banana", category: "Fruit" },
+    { id: 4, name: "Bread", category: "Other" },
+    { id: 5, name: "Broccoli", category: "Vegetable" },
+    { id: 6, name: "Cabbage", category: "Vegetable" },
+    { id: 7, name: "Mango", category: "Fruit" },
+];
